d15716cdac5e9e455ea4dddeaf13e042e3619d89
Juan Fuentes <juanf@id.ethz.ch>
2025-03-25 15:31:37 +0100

    BIS-1797: Updated Javascript library to match the Java read/write changes fixed

--- api-data-store-server-javascript/src/js/api/server-data-store-facade.js
+++ api-data-store-server-javascript/src/js/api/server-data-store-facade.js
@@ -462,17 +462,26 @@ DataStoreServer.prototype.list = function(owner, source, recursively){
  * Read the contents of selected file
  * @param {chunk} chunks of the file
  */
-DataStoreServer.prototype._read = function(owner, chunks){
+DataStoreServer.prototype._read = function(chunks){
 	const data =  this.fillCommonParameters({
 		"method": "read"
 	});
-	const result = this._internal.sendHttpRequestAbortable(
+	const original = this._internal.sendHttpRequestAbortable(
 		"POST",
 		"application/octet-stream",
 		this._internal.buildGetUrl(data),
 		ChunkEncoderDecoder.encodeChunks(chunks) // Encode Chunks
 	);
-	return ChunkEncoderDecoder.decodeChunks(result) // Decode Chunks
+
+    original.promise = original.promise.then(function(result) {
+        return result.text();
+    }).then(function(text) {
+        var chunks = ChunkEncoderDecoder.decodeChunks(text); // Decode Chunks
+        var data = chunks[0].getData();
+        return new Blob([data]);
+    });
+
+	return original;
 }
 
 /**
@@ -483,14 +492,14 @@ DataStoreServer.prototype._read = function(owner, chunks){
  * @param {int} limit how many characters to read
  */
 DataStoreServer.prototype.read = function(owner, source, offset, limit){
-	return this._read([new Chunk(owner, source, offset, limit, [])]).getData();
+	return this._read([new Chunk(owner, source, offset, limit, ChunkEncoderDecoder.EMPTY_ARRAY)]);
 }
 
 /**
  * Read the contents of selected file
  * @param {chunk} chunks of the file
  */
-DataStoreServer.prototype._write = function(owner, chunks){
+DataStoreServer.prototype._write = function(chunks){
 	const data =  this.fillCommonParameters({
 		"method": "write"
 	});
@@ -511,7 +520,7 @@ DataStoreServer.prototype._write = function(owner, chunks){
  * @param {str} data data to write
  */
 DataStoreServer.prototype.write = function(owner, source, offset, data){
-	return this._write([new Chunk(owner, source, offset, limit, data)]);
+	return this._write([new Chunk(owner, source, offset, data.length, data)]);
 }
 
 /**
@@ -687,36 +696,33 @@ DataStoreServer.prototype.recover = function(){
  * DTO
  * ==================================================================================
  */
-var ChunkEncoderDecoder = function() {
+var ChunkEncoderDecoder = (function(){
     const CHUNK_SEPARATOR = ',';
     const CHUNK_ARRAY_SEPARATOR = ';';
-    const EMPTY_ARRAY = [];
+    const EMPTY_ARRAY = new Uint8Array();
 
-    this.encodeChunk = function(chunk) {
-        const utf8decoder = new TextDecoder('utf-8');
-        const decodedText = utf8decoder.decode(chunk.getData());
-        return chunk.getOwner() + CHUNK_SEPARATOR + chunk.getSource() + CHUNK_SEPARATOR + chunk.getOffset() + CHUNK_SEPARATOR + chunk.getLimit() + CHUNK_SEPARATOR + decodedText;
+    function encodeChunk(chunk) {
+        var dataAsBase64 = base64.bytesToBase64(chunk.getData());
+        return chunk.getOwner() + CHUNK_SEPARATOR + chunk.getSource() + CHUNK_SEPARATOR + chunk.getOffset() + CHUNK_SEPARATOR + chunk.getLimit() + CHUNK_SEPARATOR + dataAsBase64;
     }
 
-    this.encodeChunks = function(chunks) {
+    function encodeChunks(chunks) {
         var builder = '';
         for (var cIdx = 0; cIdx < chunks.length; cIdx++) {
             if (cIdx > 0) {
                 builder += CHUNK_ARRAY_SEPARATOR;
             }
-            builder += this.encodeChunk(chunks[cIdx]);
+            builder += encodeChunk(chunks[cIdx]);
         }
         return builder;
     }
 
-    this.decodeChunk = function(chunkAsString) {
+    function decodeChunk(chunkAsString) {
         var chunkParameters = chunkAsString.split(CHUNK_SEPARATOR);
 
         var data = null;
         if (chunkParameters.length == 5) {
-            const textEncoder = new TextEncoder();
-            const uint8Array = textEncoder.encode(chunkParameters[4]);
-            data = uint8Array;
+            data = base64.base64ToBytes(chunkParameters[4]);
         } else {
             data = EMPTY_ARRAY;
         }
@@ -728,11 +734,11 @@ var ChunkEncoderDecoder = function() {
                 data);
     }
 
-    this.decodeChunks = function(chunksAsString) {
+    function decodeChunks(chunksAsString) {
         var chunksParameters = chunksAsString.split(CHUNK_ARRAY_SEPARATOR);
         var chunks = [];
         for (var cIdx = 0; cIdx < chunksParameters.length; cIdx++) {
-            chunks[cIdx] = this.decodeChunk(chunksParameters[cIdx]);
+            chunks[cIdx] = decodeChunk(chunksParameters[cIdx]);
         }
         return chunks;
     }
@@ -749,14 +755,19 @@ var ChunkEncoderDecoder = function() {
 //        const decodedText = utf8decoder.decode(chunksAsBytes);
 //        return this.decodeChunks(decodedText);
 //    }
-}
+    return {
+        encodeChunks : encodeChunks,
+        decodeChunks : decodeChunks,
+        EMPTY_ARRAY : EMPTY_ARRAY
+    }
+})();
 
-var Chunk = function(chunkObject) {
-    this.owner = chunkObject.owner;
-    this.source = chunkObject.source;
-    this.offset = chunkObject.offset;
-    this.limit = chunkObject.limit;
-    this.data = chunkObject.data;
+var Chunk = function(owner, source, offset, limit, data) {
+    this.owner = owner;
+    this.source = source;
+    this.offset = offset;
+    this.limit = limit;
+    this.data = data;
 
     this.getOwner = function(){
         return this.owner;
@@ -1234,4 +1245,138 @@ var { md5, md5_array } = (function(){
     }
 
     return { md5, md5_array };
+})();
+
+var base64 = (function(){
+/*
+MIT License
+Copyright (c) 2020 Egor Nepomnyaschih
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+// This constant can also be computed with the following algorithm:
+const base64abc = [],
+	A = "A".charCodeAt(0),
+	a = "a".charCodeAt(0),
+	n = "0".charCodeAt(0);
+for (let i = 0; i < 26; ++i) {
+	base64abc.push(String.fromCharCode(A + i));
+}
+for (let i = 0; i < 26; ++i) {
+	base64abc.push(String.fromCharCode(a + i));
+}
+for (let i = 0; i < 10; ++i) {
+	base64abc.push(String.fromCharCode(n + i));
+}
+base64abc.push("+");
+base64abc.push("/");
+*/
+const base64abc = [
+	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+	"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+	"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/"
+];
+
+/*
+// This constant can also be computed with the following algorithm:
+const l = 256, base64codes = new Uint8Array(l);
+for (let i = 0; i < l; ++i) {
+	base64codes[i] = 255; // invalid character
+}
+base64abc.forEach((char, index) => {
+	base64codes[char.charCodeAt(0)] = index;
+});
+base64codes["=".charCodeAt(0)] = 0; // ignored anyway, so we just need to prevent an error
+*/
+const base64codes = [
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 62, 255, 255, 255, 63,
+	52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 255, 255, 255, 0, 255, 255,
+	255, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+	15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 255, 255, 255, 255, 255,
+	255, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+	41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
+];
+
+function getBase64Code(charCode) {
+	if (charCode >= base64codes.length) {
+		throw new Error("Unable to parse base64 string.");
+	}
+	const code = base64codes[charCode];
+	if (code === 255) {
+		throw new Error("Unable to parse base64 string.");
+	}
+	return code;
+}
+
+function bytesToBase64(bytes) {
+	let result = '', i, l = bytes.length;
+	for (i = 2; i < l; i += 3) {
+		result += base64abc[bytes[i - 2] >> 2];
+		result += base64abc[((bytes[i - 2] & 0x03) << 4) | (bytes[i - 1] >> 4)];
+		result += base64abc[((bytes[i - 1] & 0x0F) << 2) | (bytes[i] >> 6)];
+		result += base64abc[bytes[i] & 0x3F];
+	}
+	if (i === l + 1) { // 1 octet yet to write
+		result += base64abc[bytes[i - 2] >> 2];
+		result += base64abc[(bytes[i - 2] & 0x03) << 4];
+		result += "==";
+	}
+	if (i === l) { // 2 octets yet to write
+		result += base64abc[bytes[i - 2] >> 2];
+		result += base64abc[((bytes[i - 2] & 0x03) << 4) | (bytes[i - 1] >> 4)];
+		result += base64abc[(bytes[i - 1] & 0x0F) << 2];
+		result += "=";
+	}
+	return result;
+}
+
+function base64ToBytes(str) {
+	if (str.length % 4 !== 0) {
+		throw new Error("Unable to parse base64 string.");
+	}
+	const index = str.indexOf("=");
+	if (index !== -1 && index < str.length - 2) {
+		throw new Error("Unable to parse base64 string.");
+	}
+	let missingOctets = str.endsWith("==") ? 2 : str.endsWith("=") ? 1 : 0,
+		n = str.length,
+		result = new Uint8Array(3 * (n / 4)),
+		buffer;
+	for (let i = 0, j = 0; i < n; i += 4, j += 3) {
+		buffer =
+			getBase64Code(str.charCodeAt(i)) << 18 |
+			getBase64Code(str.charCodeAt(i + 1)) << 12 |
+			getBase64Code(str.charCodeAt(i + 2)) << 6 |
+			getBase64Code(str.charCodeAt(i + 3));
+		result[j] = buffer >> 16;
+		result[j + 1] = (buffer >> 8) & 0xFF;
+		result[j + 2] = buffer & 0xFF;
+	}
+	return result.subarray(0, result.length - missingOctets);
+}
+
+return {
+    bytesToBase64 : bytesToBase64,
+    base64ToBytes : base64ToBytes
+};
+
 })();
