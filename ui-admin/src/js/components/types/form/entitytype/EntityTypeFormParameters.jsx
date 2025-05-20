8fc5aa2fc573c8c79533da4f20bf29a59180c7d6
Mihai Cosmin Danaila <mihaicosmin.danaila@ethz.ch>
2025-05-20 15:13:39 +0200

    BIS-1930: added tabs to EntityTypeFormParameters

--- ui-admin/src/js/components/types/form/entitytype/EntityTypeFormParameters.jsx
+++ ui-admin/src/js/components/types/form/entitytype/EntityTypeFormParameters.jsx
@@ -2,14 +2,35 @@ import React from 'react'
 import withStyles from '@mui/styles/withStyles';
 import EntityTypeFormParametersType from '@src/js/components/types/form/entitytype/EntityTypeFormParametersType.jsx'
 import EntityTypeFormParametersProperty from '@src/js/components/types/form/entitytype/EntityTypeFormParametersProperty.jsx'
+import EntityTypeFormParametersSemanticAnnotation from '@src/js/components/types/form/entitytype/EntityTypeFormParametersSemanticAnnotation.jsx';
 import EntityTypeFormParametersSection from '@src/js/components/types/form/entitytype/EntityTypeFormParametersSection.jsx'
 import logger from '@src/js/common/logger.js'
+import { Tab } from '@mui/material';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
 
-const styles = () => ({})
+const styles = theme => ({
+  tabsRoot: {
+    borderBottomStyle: 'solid',
+    borderBottomWidth: '1px',
+    borderBottomColor: theme.palette.border.primary,
+  },
+  tabPanelRoot: {
+    padding: 0,
+  }
+})
 
 class EntityTypeFormParameters extends React.PureComponent {
   constructor(props) {
     super(props)
+    this.state = {
+      tabSelected: 0
+    }
+  }
+
+  handleTabChange = (event, newValue) => {
+    this.setState({
+      tabSelected: newValue,
+    })
   }
 
   render() {
@@ -24,39 +45,78 @@ class EntityTypeFormParameters extends React.PureComponent {
       mode,
       onChange,
       onSelectionChange,
-      onBlur
+      onBlur,
+      classes
     } = this.props
 
+    const { tabSelected } = this.state;
+
+    const PROPERTIES_TAB_INDEX = 0;
+    const SEMANTIC_ANNOTATIONS_TAB_INDEX = 1;
+
     return (
-      <div>
-        <EntityTypeFormParametersType
-          controller={controller}
-          type={type}
-          selection={selection}
-          mode={mode}
-          onChange={onChange}
-          onSelectionChange={onSelectionChange}
-          onBlur={onBlur}
-        />
-        <EntityTypeFormParametersSection
-          sections={sections}
-          selection={selection}
-          mode={mode}
-          onChange={onChange}
-          onSelectionChange={onSelectionChange}
-          onBlur={onBlur}
-        />
-        <EntityTypeFormParametersProperty
-          controller={controller}
-          type={type}
-          properties={properties}
-          selection={selection}
-          mode={mode}
-          onChange={onChange}
-          onSelectionChange={onSelectionChange}
-          onBlur={onBlur}
-        />
-      </div>
+      <TabContext value={tabSelected} >
+        <TabList variant="fullWidth"
+          onChange={this.handleTabChange}
+          classes={{ root: classes.tabsRoot }}
+          textColor='inherit'
+          indicatorColor='secondary'
+          TabIndicatorProps={{
+            style: {
+              transition: 'none',
+            }
+          }}>
+          <Tab key='property-tab-id'
+            value={PROPERTIES_TAB_INDEX}
+            label="Parameters"
+          />
+          <Tab key='property-semantic-ann-tab-id'
+            value={SEMANTIC_ANNOTATIONS_TAB_INDEX}
+            label="Semantic Annotations"
+          />
+        </TabList>
+        <TabPanel classes={{ root: classes.tabPanelRoot }} value={PROPERTIES_TAB_INDEX}>
+          <EntityTypeFormParametersType
+            controller={controller}
+            type={type}
+            selection={selection}
+            mode={mode}
+            onChange={onChange}
+            onSelectionChange={onSelectionChange}
+            onBlur={onBlur}
+          />
+          <EntityTypeFormParametersSection
+            sections={sections}
+            selection={selection}
+            mode={mode}
+            onChange={onChange}
+            onSelectionChange={onSelectionChange}
+            onBlur={onBlur}
+          />
+          <EntityTypeFormParametersProperty
+            controller={controller}
+            type={type}
+            properties={properties}
+            selection={selection}
+            mode={mode}
+            onChange={onChange}
+            onSelectionChange={onSelectionChange}
+            onBlur={onBlur}
+          />
+        </TabPanel>
+        <TabPanel classes={{ root: classes.tabPanelRoot }} value={SEMANTIC_ANNOTATIONS_TAB_INDEX}>
+          <EntityTypeFormParametersSemanticAnnotation
+            controller={controller}
+            type={type}
+            properties={properties}
+            selection={selection}
+            mode={mode}
+            onChange={onChange}
+            onSelectionChange={onSelectionChange}
+            onBlur={onBlur}
+          />
+        </TabPanel>
+      </TabContext>
     )
   }
 }
