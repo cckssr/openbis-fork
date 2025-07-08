3c5012943f1cf5f3782d0226e062c427f157016c
Juan Fuentes <juanf@id.ethz.ch>
2025-07-08 16:04:07 +0200

    BIS-1961: Connection to openBIS

--- server-ro-crate/src/main/java/ch/ethz/sis/openbis/generic/server/asapi/openapi/v1/service/RoCrateImportService.java
+++ server-ro-crate/src/main/java/ch/ethz/sis/openbis/generic/server/asapi/openapi/v1/service/RoCrateImportService.java
@@ -1,5 +1,8 @@
 package ch.ethz.sis.openbis.generic.server.asapi.openapi.v1.service;
 
+import ch.ethz.sis.openbis.generic.OpenBIS;
+import ch.ethz.sis.openbis.ros.startup.RoCrateServerParameter;
+import ch.ethz.sis.openbis.ros.startup.StartupMain;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.POST;
@@ -16,12 +19,34 @@ public class RoCrateImportService {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Path("test")
+    public String test() {
+        String openBISUrl = StartupMain.getConfiguration().getStringProperty(RoCrateServerParameter.openBISUrl);
+        Integer openBISTimeout = StartupMain.getConfiguration().getIntegerProperty(RoCrateServerParameter.openBISTimeout);
+        String openBISUser = StartupMain.getConfiguration().getStringProperty(RoCrateServerParameter.openBISUser);
+        String openBISPassword = StartupMain.getConfiguration().getStringProperty(RoCrateServerParameter.openBISPassword);
+        OpenBIS openBIS = new OpenBIS(openBISUrl, openBISTimeout);
+        try {
+            return openBIS.login(openBISUser, openBISPassword);
+        } catch (Exception ex) {
+
+        } finally {
+            openBIS.logout();
+        }
+
+        return "error";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("import")
     public List<String> importRoCrate(InputStream inputStream) {
         return null;
     }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Path("export")
     public OutputStream exportRoCrate(List<String> identifiers) {
         return null;
     }
---
+++ server-ro-crate/src/main/java/ch/ethz/sis/openbis/ros/startup/StartupMain.java
@@ -0,0 +1,33 @@
+package ch.ethz.sis.openbis.ros.startup;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class StartupMain implements QuarkusApplication
+{
+    private static Configuration configuration;
+
+    public static Configuration getConfiguration() {
+        return configuration;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Quarkus.run(StartupMain.class, args);
+    }
+
+    @Override
+    public int run(String... args) throws Exception
+    {
+        System.out.println("Current Working Directory: " + (new File("")).getCanonicalPath());
+        System.out.println("Configuration Location: " + (new File(args[0])).getCanonicalPath());
+        configuration = new Configuration(List.of(RoCrateServerParameter.class), args[0]);
+
+        System.out.println(">> Quarkus app running. Press Ctrl+C to exit.");
+        Quarkus.waitForExit();
+        return 0;
+    }
+}
