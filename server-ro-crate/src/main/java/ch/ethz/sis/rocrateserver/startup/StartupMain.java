41092cbac59c49b7116c452e50a2a7fc203cfa7e
Juan Fuentes <juanf@id.ethz.ch>
2025-08-04 12:37:49 +0200

    BIS-1961: Remove application.properties, quarkus configuration simplified

--- server-ro-crate/src/main/java/ch/ethz/sis/rocrateserver/startup/StartupMain.java
+++ server-ro-crate/src/main/java/ch/ethz/sis/rocrateserver/startup/StartupMain.java
@@ -22,9 +22,12 @@ public class StartupMain implements QuarkusApplication
 
     public static void main(String[] args) throws IOException {
         System.out.println("Current Working Directory: " + (new File("")).getCanonicalPath());
-        System.out.println("Configuration Location: " + (new File(args[0])).getCanonicalPath());
-        configuration = new Configuration(List.of(RoCrateServerParameter.class), args[0]);
-        validate(configuration);
+
+        if (args != null && args.length > 0) {
+            System.out.println("Loading configuration from: " + (new File(args[0])).getCanonicalPath());
+            configuration = new Configuration(List.of(RoCrateServerParameter.class), args[0]);
+            validate(configuration);
+        }
 
         System.setProperty("quarkus.http.port", configuration.getStringProperty(RoCrateServerParameter.httpServerPort));
         System.setProperty("quarkus.http.test-port", configuration.getStringProperty(RoCrateServerParameter.httpServerPort));
@@ -37,23 +40,18 @@ public class StartupMain implements QuarkusApplication
         Quarkus.run(StartupMain.class, args);
     }
 
-    @Override
-    public int run(String... args) throws Exception
-    {
-//
-//        System.setProperty("quarkus.transaction-manager.default-transaction-timeout", configuration.getStringProperty(RoCrateServerParameter.httpServerTimeout));
-//        System.setProperty("quarkus.rest-client.connect-timeout", configuration.getStringProperty(RoCrateServerParameter.httpServerTimeout));
-//        System.setProperty("quarkus.rest-client.read-timeout", configuration.getStringProperty(RoCrateServerParameter.httpServerTimeout));
-
-        System.out.println(">> Quarkus app running. Press Ctrl+C to exit.");
-        Quarkus.waitForExit();
-        return 0;
-    }
-
     private static void validate(Configuration configuration) throws IllegalArgumentException {
         if (configuration.getStringProperty(RoCrateServerParameter.sessionWorkSpace) == null)
         {
             throw new IllegalArgumentException("Setting a session workspace is mandatory! ");
         }
+    }
+
+    @Override
+    public int run(String... args) throws Exception
+    {
+        System.out.println(">> Quarkus app running. Press Ctrl+C to exit.");
+        Quarkus.waitForExit();
+        return 0;
     }
 }
