e2b81bb28fc981fed3a7d01ff6d48e0ac998e633
Juan Fuentes <juanf@id.ethz.ch>
2025-08-05 14:41:53 +0200

    BIS-2082: Service moved to admin ui

--- ui-admin/src/core-plugins/admin/1/as/services/chat-bot-api/script.py
+++ ui-admin/src/core-plugins/admin/1/as/services/chat-bot-api/script.py
@@ -1,3 +1,7 @@
+from ch.systemsx.cisd.openbis.generic.server import CommonServiceProvider
+
+chatBotLlmServerUrl = CommonServiceProvider.tryToGetProperty("admin.as.chat-bot-api.chat-bot-llm-server-url", "https://chat-bot-server:8080/query")
+
 def process(context, parameters):
     method = parameters.get("method")
     result = None
@@ -13,7 +17,7 @@ def getAsk(context, parameters):
     if session_id is None:
         session_id = "test-session-id"
 
-    answer = "This is your answer with " + str(session_id) + " to: " + str(message)
+    answer = "This is your answer with " + str(session_id) + " and " + str(chatBotLlmServerUrl) + " to: " + str(message)
     return {
     "answer" : answer,
     "sessionId" : session_id
