09e99afaa353ec7768de3c54975a225b70a7cabb
Andreas Meier <andreas.meier@ethz.ch>
2025-12-09 12:52:41 +0100

    BIS-2358: Clean session workspace on startup

--- server-ro-crate/src/main/java/ch/ethz/sis/rocrateserver/startup/StartupMain.java
+++ server-ro-crate/src/main/java/ch/ethz/sis/rocrateserver/startup/StartupMain.java
@@ -4,10 +4,14 @@ import io.quarkus.runtime.LaunchMode;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Stream;
 
 @QuarkusMain
 public class StartupMain implements QuarkusApplication
@@ -33,6 +37,8 @@ public class StartupMain implements QuarkusApplication
             System.out.println("Loading configuration from: " + (new File(args[0])).getCanonicalPath());
             configuration = new Configuration(List.of(RoCrateServerParameter.class), args[0]);
             validate(configuration);
+            cleanBeforeRun(configuration);
+
         }
 
         System.setProperty("quarkus.http.port", configuration.getStringProperty(RoCrateServerParameter.httpServerPort));
@@ -64,4 +70,30 @@ public class StartupMain implements QuarkusApplication
         Quarkus.waitForExit();
         return 0;
     }
+
+    private static void cleanBeforeRun(Configuration configuration) throws IOException
+    {
+        if (LaunchMode.current().isDevOrTest())
+        {
+            // Locally, we might want to keep outputs from tests and such for the moment.
+            return;
+        }
+
+        Path sessionWorkSpacePath =
+                Path.of(configuration.getStringProperty(RoCrateServerParameter.sessionWorkSpace));
+        Stream<Path> list = Files.list(sessionWorkSpacePath);
+        list.forEach(x -> {
+            try
+            {
+
+                FileUtils.deleteDirectory(new File(x.toString()));
+            } catch (IOException e)
+            {
+                throw new RuntimeException(e);
+            }
+
+        });
+
+    }
+
 }
