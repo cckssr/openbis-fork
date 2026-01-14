d6a3b79bdf4b747c088d6036a2fa2e8c4584b331
Juan Fuentes <juanf@id.ethz.ch>
2026-01-14 16:07:25 +0100

    BIS-2348: First version of the custom plugin for jupyter notebooks

--- ui-eln-lims/src/core-plugins/eln-lims/1/as/webapps/eln-lims/html/plugins/template-jupyter-notebook/plugin.js
+++ ui-eln-lims/src/core-plugins/eln-lims/1/as/webapps/eln-lims/html/plugins/template-jupyter-notebook/plugin.js
@@ -7,13 +7,20 @@ $.extend(CustomJupyterNotebook.prototype, ELNLIMSPlugin.prototype, {
 
     },
     sampleTypeDefinitionsExtension: {
+        "SHOW_ON_NAV": true,
+        "SHOW": false,
         "CUSTOM_SAMPLE_TYPE": {
             extraToolbarDropdown : function(mode, sample) {
                 return {
                     label:"Custom Jupyter Notebook",
                     title:"Custom Jupyter Notebook",
                     action : function() {
-                        alert("Test");
+                        var customContent = [];
+                        customContent.push(JupyterUtil.getMarkdownCell("# Custom Title\n"));
+                        customContent.push(JupyterUtil.getMarkdownCell("Custom Paragraph"));
+                        customContent.push(JupyterUtil.getCodeCell(["custom_variable='value'"], "custom_variable"));
+                        var jupyterNotebook = new JupyterNotebookController(sample, customContent);
+                        jupyterNotebook.init();
                     }
                 }
             }
