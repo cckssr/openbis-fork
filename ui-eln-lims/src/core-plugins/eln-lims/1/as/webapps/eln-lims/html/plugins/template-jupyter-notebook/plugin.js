f85cae276deb1251c5a0ccd7953ea4b3858b5619
Juan Fuentes <juanf@id.ethz.ch>
2026-01-14 16:31:12 +0100

    BIS-2348: First version of the custom plugin for jupyter notebooks

--- ui-eln-lims/src/core-plugins/eln-lims/1/as/webapps/eln-lims/html/plugins/template-jupyter-notebook/plugin.js
+++ ui-eln-lims/src/core-plugins/eln-lims/1/as/webapps/eln-lims/html/plugins/template-jupyter-notebook/plugin.js
@@ -15,12 +15,17 @@ $.extend(CustomJupyterNotebook.prototype, ELNLIMSPlugin.prototype, {
                     label:"Custom Jupyter Notebook",
                     title:"Custom Jupyter Notebook",
                     action : function() {
-                        var customContent = [];
-                        customContent.push(JupyterUtil.getMarkdownCell("# Custom Title\n"));
-                        customContent.push(JupyterUtil.getMarkdownCell("Custom Paragraph"));
-                        customContent.push(JupyterUtil.getCodeCell(["custom_variable='value'"], "custom_variable"));
-                        var jupyterNotebook = new JupyterNotebookController(sample, customContent);
-                        jupyterNotebook.init();
+                        fetch('/openbis-test/webapp/eln-lims/plugins/template-jupyter-notebook/custom_python_notebook_cells.ipynb').then(r => r.json()).then(customContent => {
+                            var jupyterNotebook = new JupyterNotebookController(sample, customContent);
+                            jupyterNotebook.init();
+                        });
+
+                        // var customContent = [];
+                        // customContent.push(JupyterUtil.getMarkdownCell("# Custom Title\n"));
+                        // customContent.push(JupyterUtil.getMarkdownCell("Custom Paragraph"));
+                        // customContent.push(JupyterUtil.getCodeCell(["custom_variable='value'"], "custom_variable"));
+                        // var jupyterNotebook = new JupyterNotebookController(sample, customContent);
+                        // jupyterNotebook.init();
                     }
                 }
             }
