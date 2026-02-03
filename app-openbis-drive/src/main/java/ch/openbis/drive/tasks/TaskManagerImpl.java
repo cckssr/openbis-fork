a5cf45a60b8f7e390bad607ab73a0fc2ebe2cd39
Andrea Invernizzi <andrea.invernizzi@ethz.ch>
2026-02-03 16:57:27 +0100

    BIS-2277: openbis-drive: improve detection of local changes through filesystem-watches

--- app-openbis-drive/src/main/java/ch/openbis/drive/tasks/TaskManagerImpl.java
+++ app-openbis-drive/src/main/java/ch/openbis/drive/tasks/TaskManagerImpl.java
@@ -5,13 +5,15 @@ import ch.openbis.drive.db.SyncJobEventDAO;
 import ch.openbis.drive.model.SyncJob;
 import ch.openbis.drive.notifications.NotificationManager;
 import ch.openbis.drive.settings.SettingsManager;
+import ch.openbis.drive.util.DirectoryWatch;
 import lombok.NonNull;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class TaskManagerImpl implements TaskManager {
-    final ConcurrentHashMap<SyncJob, Timer> jobTimers = new ConcurrentHashMap<>();
+    final ConcurrentHashMap<SyncJob, CloseableSyncTimer> jobTimers = new ConcurrentHashMap<>();
     final ConcurrentHashMap<SyncJob, SyncOperation> syncOperations = new ConcurrentHashMap<>();
 
     private final @NonNull SyncJobEventDAO syncJobEventDAO;
@@ -31,7 +33,7 @@ public class TaskManagerImpl implements TaskManager {
 
     public synchronized void clear() {
         for (SyncJob syncJob: jobTimers.keySet()) {
-            Timer timer = jobTimers.get(syncJob);
+            CloseableSyncTimer timer = jobTimers.get(syncJob);
             timer.cancel();
             SyncOperation syncOperation = syncOperations.remove(syncJob);
             if (syncOperation != null) {
@@ -45,8 +47,12 @@ public class TaskManagerImpl implements TaskManager {
     @Override
     public synchronized void addSyncJobs(@NonNull List<@NonNull SyncJob> syncJobs, int periodSeconds) {
         syncJobs.stream().filter( SyncJob::isEnabled ).forEach( syncJob -> {
-            Timer timer = new Timer();
-            SyncJobTimeTask syncJobTimeTask = new SyncJobTimeTask(syncJob);
+            DirectoryWatch directoryWatch = new DirectoryWatch(
+                    syncJob.getLocalDirectoryRoot());
+            CloseableSyncTimer timer = new CloseableSyncTimer(directoryWatch);
+
+            SyncJobTimeTask syncJobTimeTask = new SyncJobTimeTask(syncJob, timer);
+
             timer.schedule(syncJobTimeTask, 0L, periodSeconds * 1000L);
             jobTimers.put(syncJob, timer);
         });
@@ -56,7 +62,7 @@ public class TaskManagerImpl implements TaskManager {
     public synchronized void removeSyncJobs(@NonNull List<@NonNull SyncJob> syncJobs) {
         List<SyncJob> deleted = new ArrayList<>();
         for (SyncJob syncJob: syncJobs) {
-            Timer timer = jobTimers.get(syncJob);
+            CloseableSyncTimer timer = jobTimers.get(syncJob);
             if (timer != null ) {
                 timer.cancel();
             }
@@ -70,29 +76,127 @@ public class TaskManagerImpl implements TaskManager {
     }
 
     private class SyncJobTimeTask extends TimerTask {
-        private final SyncJob syncJob;
+        static final long GRACE_PERIOD_MILLISECONDS = 15000;
+        enum TriggeringCause {
+            PERIODIC_CHECK,
+            FILESYSTEM_EVENT,
+            FILESYSTEM_EVENT_CHECK_AFTER_GRACE_PERIOD
+        }
 
-        public SyncJobTimeTask(SyncJob syncJob) {
+        private final SyncJob syncJob;
+        private final CloseableSyncTimer timer;
+        private final DirectoryWatch directoryWatch;
+
+        public SyncJobTimeTask(SyncJob syncJob, CloseableSyncTimer timer) {
             this.syncJob = syncJob;
+            this.timer = timer;
+            this.directoryWatch = timer.getDirectoryWatch();
         }
 
         @Override
         public void run() {
+            tryToInsertAndStartNewSyncOperation(TriggeringCause.PERIODIC_CHECK);
+        }
 
+        private Void tryToInsertAndStartNewSyncOperation(@NonNull TriggeringCause triggeringCause) {
+            System.out.println(String.format("Sync-job %s from %s", syncJob.getLocalDirectoryRoot(), triggeringCause));
+
+            SyncOperation syncTaskOperation;
             try {
-
-                System.out.println(String.format("Sync-job %s", syncJob.getLocalDirectoryRoot()));
-                SyncOperation syncTaskOperation = new SyncOperation(syncJob, syncJobEventDAO, notificationManager, configuration, settingsManager.getSettings());
-                syncOperations.put(syncJob, syncTaskOperation);
-                syncTaskOperation.start();
-
+                syncTaskOperation = new SyncOperation(syncJob, syncJobEventDAO, notificationManager, configuration, settingsManager.getSettings());
             } catch (Exception e) {
-                //TODO better logging system?
                 e.printStackTrace();
-            } finally {
-                syncOperations.remove(syncJob);
+                return null;
             }
+            if (syncOperations.putIfAbsent(syncJob, syncTaskOperation) == null) {
+                try {
+                    directoryWatch.close();
 
+                    if (canStartImmediately(triggeringCause)) {
+                        syncTaskOperation.start();
+                    } else {
+                        rescheduleSyncOperationAfterGracePeriod();
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    tryToRestartDirectoryWatch();
+                    syncOperations.remove(syncJob);
+                }
+            }
+            return null;
+        }
+
+        private boolean canStartImmediately(@NonNull TriggeringCause triggeringCause) {
+            return switch (triggeringCause) {
+                case PERIODIC_CHECK -> true;
+                case FILESYSTEM_EVENT -> false;
+                case FILESYSTEM_EVENT_CHECK_AFTER_GRACE_PERIOD ->
+                        System.currentTimeMillis() - directoryWatch.getLastEventTs() > GRACE_PERIOD_MILLISECONDS - 1000;
+            };
+        }
+
+        private Void rescheduleSyncOperationFromFileSystemEvent() {
+            if ( !timer.isCancelled() ) {
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        tryToInsertAndStartNewSyncOperation(TriggeringCause.FILESYSTEM_EVENT);
+                    }
+                }, 0);
+            }
+            return null;
+        }
+
+        private Void rescheduleSyncOperationAfterGracePeriod() {
+            if ( !timer.isCancelled() ) {
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        tryToInsertAndStartNewSyncOperation(TriggeringCause.FILESYSTEM_EVENT_CHECK_AFTER_GRACE_PERIOD);
+                    }
+                }, GRACE_PERIOD_MILLISECONDS);
+            }
+            return null;
+        }
+
+        private void tryToRestartDirectoryWatch() {
+            try {
+                if (syncJob.getType() == SyncJob.Type.Upload || syncJob.getType() == SyncJob.Type.Bidirectional) {
+                    directoryWatch.start(this::rescheduleSyncOperationFromFileSystemEvent);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class CloseableSyncTimer extends Timer {
+        final AtomicBoolean cancelled = new AtomicBoolean(false);
+        final DirectoryWatch directoryWatch;
+
+        public CloseableSyncTimer(@NonNull DirectoryWatch directoryWatch) {
+            this.directoryWatch = directoryWatch;
+        }
+
+        public DirectoryWatch getDirectoryWatch() {
+            return directoryWatch;
+        }
+
+        @Override
+        public void cancel() {
+            this.cancelled.set(true);
+            try {
+                this.directoryWatch.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            super.cancel();
+        }
+
+        public boolean isCancelled() {
+            return cancelled.get();
         }
     }
 
