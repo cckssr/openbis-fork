76d7a78633dd0b0a64fc1d4f143d7d1e7f629da3
Tufail Tak <tuftak@ethz.ch>
2026-02-04 11:10:08 +0100

    BIS-2025: Create a New DB Version

--- server-application-server/resource/scripts/create_new_db_version.sh
+++ server-application-server/resource/scripts/create_new_db_version.sh
@@ -4,9 +4,17 @@
 # Assumes that it is called from the directory where it is located.
 
 SQL_DIR=../../source/sql/openbis
+echo "Copy Source files $SQL_DIR"
 source common.sh
 
 copy_db_folder generic
 copy_db_folder postgresql
 copy_migration_file
+print_finish_message
+
+#Copy also the test
+SQL_DIR=../../sourceTest/sql/openbis
+echo "Copy Test files $SQL_DIR"
+source common.sh
+copy_db_folder postgresql
 print_finish_message
