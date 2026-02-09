b2bdb28786bf89420b778e92daf1e0ad8c18b633
alaskowski <alaskowski@ethz.ch>
2026-02-09 13:50:10 +0100

    BIS-2256: Added exception to be thrown when AFS is not available in XlsImport

--- server-application-server/source/java/ch/ethz/sis/openbis/generic/server/asapi/v3/executor/importer/ImportExecutor.java
+++ server-application-server/source/java/ch/ethz/sis/openbis/generic/server/asapi/v3/executor/importer/ImportExecutor.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright ETH 2023 Zürich, Scientific IT Services
+ *  Copyright ETH 2023-2026 Zürich, Scientific IT Services
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
