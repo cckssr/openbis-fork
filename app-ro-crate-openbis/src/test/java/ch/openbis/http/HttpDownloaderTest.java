04bdc0b1ab846218e441f2d544303ad4331697e2
Juan Fuentes <juanf@id.ethz.ch>
2026-03-13 12:15:42 +0100

    BIS-2557: Prototype of a concurrent http downloader for manage downloads on external ro-crate resources

--- app-ro-crate-openbis/src/test/java/ch/openbis/http/HttpDownloaderTest.java
+++ app-ro-crate-openbis/src/test/java/ch/openbis/http/HttpDownloaderTest.java
@@ -1,4 +1,48 @@
 package ch.openbis.http;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.nio.file.Files;import java.nio.file.Path;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class HttpDownloaderTest {
+    @Test
+    public void apiErrorTest() throws Exception {
+        AtomicReference<Exception> expected = new AtomicReference<>();
+
+        HttpDownloader httpDownloader = new HttpDownloader();
+        httpDownloader.add("non sense url to test the error handler", Path.of("non sense path to test the error handler"));
+
+        httpDownloader.error((url, path, exception) -> {
+            expected.set(exception);
+        }).override((url, path) -> {
+            return true;
+        }).start();
+
+        assertNotNull(expected.get());
+    }
+
+    @Test
+    public void apiDownloadTest() throws Exception {
+        Path tempDir = Path.of(System.getProperty("java.io.tmpdir"));
+        Path tempFile = tempDir.resolve("google.com.tmp");
+        if (Files.exists(tempFile)) {
+            Files.delete(tempFile);
+        }
+        AtomicReference<Exception> expected = new AtomicReference<>();
+
+        HttpDownloader httpDownloader = new HttpDownloader();
+        httpDownloader.add("http://google.com", tempFile);
+
+        httpDownloader.error((url, path, exception) -> {
+            expected.set(exception);
+        }).override((url, path) -> {
+            return true;
+        }).start();
+
+        assertNull(expected.get());
+        assertTrue(Files.exists(tempFile));
+        assertTrue(Files.size(tempFile) > 0);
+    }
 }
