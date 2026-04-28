2861cb5bdaa2f05c060dbc2eda5ca277cd1e8baf
Juan Fuentes <juanf@id.ethz.ch>
2026-04-28 11:18:00 +0200

    BIS-2647: Sample Data Type identifier test fixes

--- app-ro-crate-openbis/src/test/java/ch/openbis/rocrate/app/reader/helper/DataTypeMatcherTest.java
+++ app-ro-crate-openbis/src/test/java/ch/openbis/rocrate/app/reader/helper/DataTypeMatcherTest.java
@@ -7,31 +7,33 @@ import org.junit.Test;
 public class DataTypeMatcherTest
 {
     @Test
+    public void onePartIdentifierTest()
+    {
+        Assert.assertFalse(DataTypeMatcher.matches("/SHARED_SAMPLE_NOT_ALLOWED", DataType.SAMPLE));
+    }
+
+    @Test
     public void twoPartIdentifierTest()
     {
-        Assert.assertTrue(DataTypeMatcher.matches("MATERIALS/ENTRY1", DataType.SAMPLE));
-
+        Assert.assertTrue(DataTypeMatcher.matches("/SPACE_CODE/SPACE_SAMPLE_CODE", DataType.SAMPLE));
     }
 
     @Test
     public void threePartIdentifierTest()
     {
-        Assert.assertTrue(DataTypeMatcher.matches("MATERIALS/ENTRY1/A", DataType.SAMPLE));
-
+        Assert.assertTrue(DataTypeMatcher.matches("/SPACE_CODE/PROJECT_CODE/SAMPLE_CODE", DataType.SAMPLE));
     }
 
     @Test
-    public void fourPartIdentifierTest()
+    public void threePartIdentifierWithContainerTest()
     {
-        Assert.assertTrue(DataTypeMatcher.matches("MATERIALS/ENTRY1/A/A", DataType.SAMPLE));
-
+        Assert.assertTrue(DataTypeMatcher.matches("/SPACE_CODE/PROJECT_CODE/CONTAINER_SAMPLE_CODE:CONTAINED_SAMPLE_CODE", DataType.SAMPLE));
     }
 
     @Test
-    public void fivePartIdentiferTest()
+    public void fourPartIdentiferTest()
     {
-        Assert.assertFalse(DataTypeMatcher.matches("MATERIALS/ENTRY1/A/A/A", DataType.SAMPLE));
-
+        Assert.assertFalse(DataTypeMatcher.matches("/SPACE_CODE/PROJECT_CODE/SAMPLE_CODE/DOESNT_EXIT", DataType.SAMPLE));
     }
 
 }
