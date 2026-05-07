27b7f614a902b5e9a0e0bc7387aed49f30b37b06
alaskowski <alaskowski@ethz.ch>
2026-05-07 09:42:39 +0200

    BIS-2431: Fixed tests

--- test-api-openbis-javascript/servers/common/core-plugins/tests/1/as/webapps/openbis-test/html/openbis-test.js
+++ test-api-openbis-javascript/servers/common/core-plugins/tests/1/as/webapps/openbis-test/html/openbis-test.js
@@ -642,7 +642,7 @@ test("listDataSetTypes()", function() {
 test("listSampleTypes()", function() {
 	createFacadeAndLogin(function(facade) {
 		facade.listSampleTypes(function(response) {
-			assertObjectsCount(response.result, 28);
+			assertObjectsCount(response.result, 31);
 			facade.close();
 		});
 	});
