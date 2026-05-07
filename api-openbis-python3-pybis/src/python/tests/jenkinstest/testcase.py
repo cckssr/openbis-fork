1e28696e00f113da073710893fc7715796c03f34
alaskowski <alaskowski@ethz.ch>
2026-05-07 13:05:25 +0200

    BIS-2524: added missing folders config

--- api-openbis-python3-pybis/src/python/tests/jenkinstest/testcase.py
+++ api-openbis-python3-pybis/src/python/tests/jenkinstest/testcase.py
@@ -612,10 +612,10 @@ class OpenbisController(_Controller):
         self.dssProperties['archiver.cleaner.deletion-polling-time'] = '1 sec'
         self.dssProperties['archiver.timeout'] = '10800'
 
-        os.makedirs("%s/data/archive/tmp" % self.targetFolder)
-        os.makedirs("%s/data/archive/stage" % self.targetFolder)
-        os.makedirs("%s/data/archive/final" % self.targetFolder)
-        os.makedirs("%s/data/archive/backup" % self.targetFolder)
+        os.makedirs("%s/openbis/data/archive/tmp" % self.targetFolder)
+        os.makedirs("%s/openbis/data/archive/stage" % self.targetFolder)
+        os.makedirs("%s/openbis/data/archive/final" % self.targetFolder)
+        os.makedirs("%s/openbis/data/archive/backup" % self.targetFolder)
 
         print("Target: %s", self.targetFolder)
         print("TestName: %s", self.testName)
