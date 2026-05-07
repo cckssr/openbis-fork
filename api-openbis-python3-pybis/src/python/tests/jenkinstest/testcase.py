a8e8bb8c0a6c02e54c89b7b0b9677a468d24b670
alaskowski <alaskowski@ethz.ch>
2026-05-07 13:49:34 +0200

    BIS-2524: PYBIS: added test configuration of core-plugins

--- api-openbis-python3-pybis/src/python/tests/jenkinstest/testcase.py
+++ api-openbis-python3-pybis/src/python/tests/jenkinstest/testcase.py
@@ -77,7 +77,8 @@ class TestCase(object):
         self.artifactRepository = settings.REPOSITORY
         self.project = None
         print("[TEST CONFIG] TESTCASE FILE PATH: %s" % filePath)
-        self.pluginsFolder = filePath + "/../core-plugins"
+        self.pluginsFolder = os.path.dirname(filePath) + "/core-plugins"
+        print("[TEST CONFIG] PLUGINS FOLDER PATH: %s" % self.pluginsFolder)
         fileName = os.path.basename(filePath)
         self.name = fileName[0:fileName.rfind('.')]
         self.playgroundFolder = "%s/%s" % (PLAYGROUND, self.name)
