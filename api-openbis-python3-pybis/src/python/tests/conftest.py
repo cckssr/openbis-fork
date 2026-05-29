aebf6c202fd988c56b600e99fe9241a5a409ed73
alaskowski <alaskowski@ethz.ch>
2026-05-29 09:26:54 +0200

    BIS-2737: PYBIS: setup afs in jenkins python tests

--- api-openbis-python3-pybis/src/python/tests/conftest.py
+++ api-openbis-python3-pybis/src/python/tests/conftest.py
@@ -87,7 +87,7 @@ def afs(space):
         data_store = data_stores[data_stores["code"] == "AFS"]
         print(f'[TEST] Found AFS datastore: {data_store}')
 
-        ds_list = data_store.to_list()
+        ds_list = data_store.values[0]
         print(f'[TEST] AFS list {ds_list} | {ds_list[1]}')
         url = ds_list[1]
         # url = data_stores.loc[data_stores['code'] == 'AFS', 'downloadUrl']
