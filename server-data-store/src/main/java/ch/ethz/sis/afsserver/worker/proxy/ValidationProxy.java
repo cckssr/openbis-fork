f4cb5ed86754e346a7061afdf2a723d3ce65a3ad
Juan Fuentes <juanf@id.ethz.ch>
2026-06-02 10:19:48 +0200

    BIS-2765: improving read/write transfer limits validation

--- server-data-store/src/main/java/ch/ethz/sis/afsserver/worker/proxy/ValidationProxy.java
+++ server-data-store/src/main/java/ch/ethz/sis/afsserver/worker/proxy/ValidationProxy.java
@@ -41,14 +41,13 @@ public class ValidationProxy extends AbstractProxy {
 
     @Override
     public Chunk[] read(@NonNull Chunk[] chunks) throws Exception {
-        for (Chunk chunk: chunks){
-            validateReadSize(chunk.getSource(), chunk.getLimit());
-        }
+        validateSize(chunks);
         return nextProxy.read(chunks);
     }
 
     @Override
     public Boolean write(@NonNull Chunk[] chunks) throws Exception {
+        validateSize(chunks);
         return nextProxy.write(chunks);
     }
 
@@ -106,9 +105,17 @@ public class ValidationProxy extends AbstractProxy {
         return nextProxy.status(operationId);
     }
 
-    private void validateReadSize(String source, Integer limit) {
+    private void validateSize(Chunk[] chunks) {
+        int limit = 0;
+        for (Chunk chunk: chunks) {
+            int dataLength = 0;
+            if (chunk.getData() != null) {
+                dataLength = chunk.getData().length;
+            }
+            limit += Math.max(chunk.getLimit(), dataLength) ;
+        }
         if (limit > maxReadSizeInBytes) {
-            throw FSExceptions.MAX_READ_SIZE_EXCEEDED.getInstance(workerContext.getSessionToken(), limit, source, maxReadSizeInBytes);
+            throw FSExceptions.MAX_READ_SIZE_EXCEEDED.getInstance(workerContext.getSessionToken(), limit, maxReadSizeInBytes);
         }
     }
 }
