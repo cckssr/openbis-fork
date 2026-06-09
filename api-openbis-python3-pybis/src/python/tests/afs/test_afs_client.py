495b07106b04bb39d872dbaba5d287e31fb97f96
alaskowski <alaskowski@ethz.ch>
2026-06-09 11:21:31 +0200

    BIS-2773: PYBIS: added test for detecting afs in pybis

--- api-openbis-python3-pybis/src/python/tests/afs/test_afs_client.py
+++ api-openbis-python3-pybis/src/python/tests/afs/test_afs_client.py
@@ -7,6 +7,26 @@ import pytest
 import tempfile
 import filecmp
 
+from pybis.afs import AfsClient
+
+
+def test_afs_detection(openbis_instance):
+    data_stores = openbis_instance.get_datastores(with_afs=True)
+    print(f'[TEST] Detected data stores: {data_stores}')
+    data_store = data_stores[data_stores["code"] == "AFS"]
+    print(f'[TEST] Found AFS datastore: {data_store}')
+
+    # workaround because jenkins test server is not handling DataFrame properly
+    import numpy as np
+    data_store_list = np.array(data_store).tolist()
+    print(f'[TEST] AFS data after conversion: {data_store_list}')
+
+    afs_url = data_store_list[0][1] + "/api" if len(data_store_list) > 0 else None
+
+    print(f'[TEST] Configured OpenBIS AFS url is: {afs_url}')
+    afs_client = AfsClient(afs_url, openbis_instance.token, False)
+
+    assert afs_client.is_session_valid()
 
 
 def test_afs_client(afs):
